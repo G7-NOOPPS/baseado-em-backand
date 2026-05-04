a8ba6dd1560e6865b8cd433634d129de952d8583
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-05-04 11:50:48 +0000

    v2

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,12 @@
+const cardapio = [ 
+  {nome: "x- burguer", preco: 18.90, catetegoria:"lanche"},
+  {nome: "batata-frita", preco: 9.50, categoria: "acompanhamneto"},
+  {nome: "sprite", preco: 4.50, categoria: "bebida"},
+  {nome: "milkshake", preco: 12.00, categoria: "sobremesa"}
+]
 
+const pedido = [];
+
+console.log(cardapio[0].nome); 
+console.log(cardapio[1].preco); 
+console.log(cardapio[2].categoria); 
