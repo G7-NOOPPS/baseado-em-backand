ea910198b288c6b14807f1e486a05892b9fba8fe
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-05-05 11:54:02 +0000

    v1

--- cardapio.js
+++ cardapio.js
@@ -10,3 +10,16 @@ const pedido = [];
 console.log(cardapio[0].nome); 
 console.log(cardapio[1].preco); 
 console.log(cardapio[2].categoria); 
+
+function exibirCardapio(lista) {
+  console.log("===CARDAPIO===");
+  console.log(`1. ${lista[0]} - R$ ${lista[0].preco}`)
+  console.log(`2. ${lista[0]} - R$ ${lista[1].preco}`)
+  console.log(`3. ${lista[0]} - R$ ${lista[2].preco}`)
+  console.log(`4. ${lista[0]} - R$ ${lista[3].preco}`)
+}
+
+function calcularTotal(listaPedido) {
+  let total = listaPedido[0].preco + listaPedido[1].preco;
+  return total;
+}
