3b27c1662e2152fd23df77109ea5d384c4b614c1
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-06-23 11:51:44 +0000

    v2

--- escola.js
+++ escola.js
@@ -1,11 +1,25 @@
-const DadosDoAluno = require('./aluno.json');
+const DadosDoAluno1 = require('./aluno.json');
+const DadosDoAluno2 = require('./aluno2.json');
 console.log('--- SISTEMA ESCOLAR ---');
-console.log(`nome: ${DadosDoAluno.nome}`);
-console.log(`curso: ${DadosDoAluno.curso}`);
-console.log(`idade: ${DadosDoAluno.idade}`);
 
-if (DadosDoAluno.bolsista) {
-    console.log("status:esse aluno possui isençao de mensalidade")
+console.log('\n--- ALUNO 1 ---');
+console.log(`nome: ${DadosDoAluno1.nome}`);
+console.log(`curso: ${DadosDoAluno1.curso}`);
+console.log(`idade: ${DadosDoAluno1.idade}`);
+
+if (DadosDoAluno1.bolsista) {
+    console.log("status: esse aluno possui isençao de mensalidade")
 } else {
-    console.log("estatus: mensalidade padrao")
+    console.log("status: mensalidade padrao")
+}
+
+console.log('\n--- ALUNO 2 ---');
+console.log(`nome: ${DadosDoAluno2.nome}`);
+console.log(`curso: ${DadosDoAluno2.curso}`);
+console.log(`idade: ${DadosDoAluno2.idade}`);
+
+if (DadosDoAluno2.bolsista) {
+    console.log("status: esse aluno possui isençao de mensalidade")
+} else {
+    console.log("status: mensalidade padrao")
 }
