c1ad467d84945e7de69bcf30b5ed60fdd86e3e8e
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-07-29 12:30:25 +0000

    v1]

---
+++ conta.js
@@ -0,0 +1,20 @@
+let saldoJoao = 1000
+let saldoCarlos = 500
+
+const taxaJuros = 0.02
+const saldoComJuros = saldoJoao * (taxaJuros)
+
+const contajoao = {titular: "joao", saldo: saldoJoao};
+const contaCarlos = {titular: "Carlos", saldo: saldoCarlos};
+
+console.log(`${contajoao.titular} possui r$ ${contajoao.saldo}.`);
+console.log(`${contaCarlos.titular} possui r$ ${contaCarlos.saldo}.`);
+
+const contas = [contajoao, contaCarlos]
+
+conta.forEach((conta) => {
+    console.log(`${conta.titular} tem R$ ${conta.saldo}`);
+});
+
+contajoao.saldo = -5000;
+console.log("saldo alterado sem controle: ", contajoao.saldo)
